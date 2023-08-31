e3a7283bf513be1fafa3d8266721ad75532adc62
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-08-30 19:51:17 -0600

    init commit. Hero almost done!!!

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import { 
+  Navbar,
+  Hero,
+  About,
+  Experience,
+  Tech,
+  Works,
+  Feedbacks,
+  Contact,
+  StarsCanvas
+  } from './components';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
